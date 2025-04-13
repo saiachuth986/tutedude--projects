0fab9eb776574c72d28d57eba0306492cfdc1227
saiachuth986 <sachuth986@gmail.com>
2025-04-13 14:18:54 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,8 @@
+a=input('enter the value of a:')
+b=input('enter the value of b:')
+a=int(a)
+b=int(b)
+print('addition:',a+b)
+print('subtraction:',a-b)
+print('multiplication:',a*b)
+print('division:',a/b)
