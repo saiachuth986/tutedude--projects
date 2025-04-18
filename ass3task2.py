67c579f33b681429cc00f01294bae7249745eaeb
saiachuth986 <sachuth986@gmail.com>
2025-04-18 17:14:44 +0530

    Add files via upload

---
+++ ass3task2.py
@@ -0,0 +1,13 @@
+import math
+#asking the user for a number
+number=float(input("enter the number:"))
+
+#calculating using math module
+square_root=math.sqrt(number)
+logarithm=math.log(number)
+sine=math.sin(number)
+
+#displaying the result
+print('square_root:',square_root)
+print('log:',logarithm)
+print('sine:',sine)
